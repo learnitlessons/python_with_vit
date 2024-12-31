be5ec708143a306be2e7337dd81c4742912bce2b
learnitlessons <162319132+learnitlessons@users.noreply.github.com>
2024-12-31 10:49:47 +0000

    new file:   1.numbers.py 	new file:   2.1.strings.py 	new file:   2.2.strinngs.py 	modified:   demo.py 	modified:   tmp.py

---
+++ 2.1.strings.py
@@ -0,0 +1,17 @@
+message = 'Python'
+
+print(len(message))
+
+print(message[0])
+print(message[1])
+
+message[-1]
+message[len(message)-1]
+
+print(message[-1])
+print(message[len(message)-1])
+
+message[1:3]
+print(message[1:3])
+
+
